1b0d596d6af5b13590b5a79ad074a98cbe130456
SystemGhost <jackie07364538@gmail.com>
2018-09-15 14:58:05 +0300

    advanced zeros: soleved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let j = base;
+	let p = 0;
+	let noz;
+
+	for(let i = 2; i <= base; i++){
+		if(j % i == 0) {
+			p = 0;
+			while(j % i == 0) {
+				p++;
+				j = j / i;
+			}
+			c = 0;
+			k = number;
+			while(k/i > 0) {
+				c+= Math.floor(k/i);
+				k = Math.floor(k / i);
+			}
+				noz = min(noz, c/p);
+		}
+	}
+	return Math.floor(noz);
+}
+
+function min(x, y) {
+	if(x < y) return x;
+	return y;
 }
